8464dd90c62912dd3360e0a25fc66513b46d10e7
Elena Vigo <elenavigoolivan@gmail.com>
2020-09-09 16:09:13 +0200

    apikey configuration

--- Clima/Constants.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Constants.swift
-//  Clima
-//
-//  Created by Elena Vigo Olivan on 09/09/2020.
-//  Copyright © 2020 App Brewery. All rights reserved.
-//
-
-let OPEN_WEATHER_API_KEY="e0daed5a1d575ed42abdf8dd2a37335b"
